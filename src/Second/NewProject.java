92862dbb769aca573b0fac98ee1e75123ebad5cb
Kamaljit kaur <saikamaljit181294@gmail.com>
2019-07-11 14:08:59 -0400

    Here's my first coding

---
+++ src/Second/NewProject.java
@@ -0,0 +1,19 @@
+package Second;
+
+public class NewProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+	
+		if(x%2==0)
+		{
+			System.out.println("EVEN NUMBER");
+		}
+		else
+		{
+			System.out.println("ODD NUMBER");
+		}
+	}
+
+}
